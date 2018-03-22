0a27ed3fa893332507c6ba3938c9190b5d8545b0
balintataw <balintataw2@gmail.com>
2018-03-22 12:48:16 -0700

    added images to card object

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const cardImages = ['at-at.svg','bounty_hunter.svg','c3po.svg','death_star.svg','clone.svg','yoda.svg','light_saber_green.svg','r2d2.svg','tie-fighter.svg']
+
 class Card {
 	constructor(value, display) {
 		this.value = value
@@ -8,12 +10,12 @@ class Card {
 class Deck {
 	constructor() {
 		this.cards = []
-		this.pics = ["one", "two", "three", "four", "five"]
+		// this.pics = ["one", "two", "three", "four", "five"]
 
 		for(let x = 0; x < 2; x++) {
 			for(let v = 0; v < 5; v++) {
 				let value = v
-				this.cards.push(new Card(value, this.pics[v]))
+				this.cards.push(new Card(value, cardImages[v]))
 			}
 		}
 	}
